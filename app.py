ec5a63640d0e016475333ad53d0a3d8dc979eebb
ishi03 <ishita.rai@somaiya.edu>
2021-06-17 16:10:57 +0530

    upadates

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_admin.contrib.sqla import ModelView
 import os, json
 #.\env\Scripts\activate.ps1
 from sqlalchemy.ext.automap import automap_base
+from impl2 import *
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=None
@@ -41,46 +42,6 @@ def home():
 def map():
     return render_template('routes.html')
 
-
-@app.route('/gmap',methods=['GET', 'POST'])
-def dolp():
-    try:
-        sqliteConnection = sqlite3.connect('E:\sem 4\mini project\project\coordinates.db')
-        cursor = sqliteConnection.cursor()
-        print("Connected to SQLite")
- 
-        sqlite_select_query = """SELECT x_coord, y_coord,name,description,fac_type FROM coordinates;"""
-       
-        cursor.execute(sqlite_select_query)
-       
-        items = cursor.fetchall()
-        print(items)
-        data2=[{"x_coord":19.076999,"y_coord":72.900914,"name":"somaiya vidyavihar","des":"welcome to svv"}]
-        #data=[(19.076999, 72.900914,"somaiya vidyavihar","welcome to svv","uni")]
-        #cursor.close()
-        if request.method == "POST":
-            book = request.form['book']
-            cursor.execute("SELECT x_coord, y_coord,name,description,fac_type,image FROM coordinates WHERE lower(name) like '"+str(book).lower()+"' or name  in (SELECT hints.name from hints where lower(hints.keyword) = '"+str(book).lower()+"')")
-            sqliteConnection.commit()
-            data = cursor.fetchall()
-            #data=data+data1
-            print(data)
-            data1=list()
-            for item in data:
-                item1={"x_coord":item[0],"y_coord":item[1],"name":item[2],"des":item[3]}
-                data1.append(item1)
-            print(data1)
-            return render_template('map1.html',data=data1,leng=len(data1))
-            
- 
-    except sqlite3.Error as error:
-        print("Failed to read data from sqlite table", error)
-    finally:
-        if (sqliteConnection):
-            sqliteConnection.close()
-            print("The SQLite connection is closed")
-    
-    return render_template('map1.html',data=data2,leng=len(data2))
 #ignore
 
 @app.route("/login", methods=["GET", "POST"])
@@ -97,58 +58,8 @@ def login():
 def onw():
     return render_template("geoloc.html")
 
-@app.route("/trial2", methods=["GET", "POST"])
+@app.route("/mpmaps", methods=["GET", "POST"])
 def ont():
-    try:
-        sqliteConnection = sqlite3.connect('E:\sem 4\mini project\project\coordinates.db')
-        cursor = sqliteConnection.cursor()
-        print("Connected to SQLite")
- 
-        sqlite_select_query = """SELECT x_coord, y_coord,name,description,fac_type FROM coordinates;"""
-       
-        cursor.execute(sqlite_select_query)
-       
-        items = cursor.fetchall()
-        #print(items)
-        data2=[{"x_coord":19.076999,"y_coord":72.900914,"name":"somaiya vidyavihar","des":"welcome to svv"}]
-        print(data2)
-        #data=[(19.076999, 72.900914,"somaiya vidyavihar","welcome to svv","uni")]
-        #cursor.close()
-        if request.method == "POST":
-            book = request.form['book']
-            cursor.execute("SELECT x_coord, y_coord,name,description,fac_type,image FROM coordinates WHERE lower(name) like '"+str(book).lower()+"' or name  in (SELECT hints.name from hints where lower(hints.keyword) = '"+str(book).lower()+"')")
-            sqliteConnection.commit()
-            data = cursor.fetchall()
-            #data=data+data1
-            print(data)
-            data1=list()
-            for item in data:
-                item1={"x_coord":item[0],"y_coord":item[1],"name":item[2],"des":item[3]}
-                data1.append(item1)
-            print(data1)
-            return render_template('map5.html',data=data1,leng=len(data1))
-        else:
-            return render_template('map5.html',data=data2,leng=len(data2))
- 
-    except sqlite3.Error as error:
-        print("Failed to read data from sqlite table", error)
-    finally:
-        if (sqliteConnection):
-            sqliteConnection.close()
-            print("The SQLite connection is closed")
-    
-    return render_template('map5.html',data=data2,leng=len(data2))
-
-@app.route("/trial5")
-def ond():
-    return render_template("map4.html")
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/")
-
-@app.route('/gmap1',methods=['GET', 'POST'])
-def holp():
     try:
         sqliteConnection = sqlite3.connect('E:\sem 4\mini project\project\coordinates.db')
         cursor = sqliteConnection.cursor()
@@ -159,25 +70,41 @@ def holp():
         cursor.execute(sqlite_select_query)
        
         items = cursor.fetchall()
+        #print(items)
         data2=[{"x_coord":19.076999,"y_coord":72.900914,"name":"somaiya vidyavihar","des":"welcome to svv"}]
-        
-    # data3=[(19.076999, 72.900914,"somaiya vidyavihar","welcome to svv","uni",'https://img.collegepravesh.com/2018/11/KJSCE-Mumbai.jpg')]
-    
+        print(data2)
+        cursor.execute("select sname,count(*)from log group by sname order by count(sname) desc limit 3")
+        loglist=cursor.fetchall()
+        loglist=[x[0].capitalize() for x in loglist]
+        print(loglist)
+        sugg=[]
         if request.method == "POST":
-            #data2=[{"x_coord":19.076999,"y_coord":72.900914,"name":"somaiya vidyavihar","des":"welcome to svv"}]
+            # sugg=[]
             book = request.form['book']
             cursor.execute("SELECT x_coord, y_coord,name,description,fac_type,image FROM coordinates WHERE lower(name) like '"+str(book).lower()+"' or name  in (SELECT hints.name from hints where lower(hints.keyword) = '"+str(book).lower()+"')")
             sqliteConnection.commit()
             data = cursor.fetchall()
             #data=data+data1
-            print(data+["1"])
-            data1=[(19.076999, 72.900914,"somaiya vidyavihar","welcome to svv","uni",'https://img.collegepravesh.com/2018/11/KJSCE-Mumbai.jpg')]
+            print("data:",data)
+            if len(data)==0:
+                cursor.execute("select name from coordinates")
+                datanon = cursor.fetchall()
+                prob=exec(datanon,book)
+                print(prob)
+                topp="Try searching for '"+ str(prob[0][0]) +"' instead"
+                sugg.append(topp)
+                print(sugg[0])
+            data1=list()
+            cursor.execute("insert into log(sname) values('"+str(book).lower()+"')")
+            sqliteConnection.commit()
             for item in data:
-                item1={"x_coord":item[0],"y_coord":item[1],"name":item[2],"des":item[3],"img":item[5]}
+                item1={"x_coord":item[0],"y_coord":item[1],"name":item[2],"des":item[3],"image":item[5]}
                 data1.append(item1)
-            print(data1+["2"])
-            return render_template('map3.html',data=data1,leng=len(data1),items=items)
+            print(data1)
             
+            return render_template('map6.html',data=data1,items=items,sugg=sugg,loglist=loglist)
+        else:
+            return render_template('map6.html',data=data2,items=items,sugg=sugg,loglist=loglist)
  
     except sqlite3.Error as error:
         print("Failed to read data from sqlite table", error)
@@ -185,12 +112,18 @@ def holp():
         if (sqliteConnection):
             sqliteConnection.close()
             print("The SQLite connection is closed")
-        # item1={"x_coord":19.076999,"y_coord":72.900914,"name":"somaiya vidyavihar","des":"welcome to svv"}
-        # data2.append(item1)
-    print(data2+["3"])
-    return render_template('map3.html',data=data2,leng=len(data2),items=items)
+    
+    return render_template('map6.html',data=data2,items=items)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+#.\env\Scripts\activate.ps1
